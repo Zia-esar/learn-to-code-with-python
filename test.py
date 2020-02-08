d582cc9b095d93ec34ff818a4918377984313dbb
Zia-esar <ziaesar@gmail.com>
2020-02-08 13:06:35 +0500

    added

---
+++ test.py
@@ -0,0 +1,22 @@
+
+print("hello world!")
+print("zia", "ur", "rehman", sep="--*--", end=" hello")
+print()
+print("hello", "world", sep=",", end="!")       # default values are [sep=" ", end="/n"]
+print()
+print("hello" * 5)
+print(5 ** 3)                                   # ** represent power
+
+name = "zia"
+age = 21
+hobby = "reading"
+
+print("my name is", name, "and i am ", age, "years old.", "My hobby is", hobby)
+
+a = b = 5                                       # multiple varialbe with same value
+
+a, b = 4, 10                                    # assignment of diff variables on same line
+
+#test = input("enter your name : ")
+
+#print(days_remaining/10)
